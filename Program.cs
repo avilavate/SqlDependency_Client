2ddf569350077de4967296a224bb2cfa24d08ad6
A <avilavate@live.in>
2017-12-14 16:36:33 +0530

    Added NotificationList Client (Cosnole)

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NotificaionListDB_Client
+{
+    class Program
+    {
+        private static string ans;
+
+
+        static void Main(string[] args)
+        {
+            ans = "y";
+            Console.WriteLine("Welcome...");
+            while (ans == "y")
+            {
+                Console.WriteLine("Enter Notification List Details:");
+                NotificationList row = new NotificationList();
+                Console.WriteLine("Text ?");
+                row.Text = Console.ReadLine();
+                Console.WriteLine("User Id ?");
+                row.UserId = int.Parse(Console.ReadLine().ToString());
+                Console.WriteLine("Create Date");
+                row.CreatedDate = DateTime.Parse(Console.ReadLine());
+                var insert_task = Task.Factory.StartNew(() =>
+                {
+                    using (NotificationRepo repo = new NotificationRepo())
+                    {
+                        repo.Insert(row);
+                    }
+                });
+                Task.Factory.StartNew(() =>
+                {
+                    insert_task.Wait();
+                    if (insert_task.IsCompleted)
+                    {
+                        Console.WriteLine("Insert operation done!");
+                    }
+                    else
+                        Console.WriteLine("last insert operation failed");
+                });
+
+                Console.WriteLine("Continue y/n? ");
+                ans = Console.ReadLine();
+            }
+
+        }
+    }
+}
